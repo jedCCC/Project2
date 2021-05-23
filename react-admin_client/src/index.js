08f09a37f23953795738c849920663cfde4aa814
jedCCC <a535297316@gmail.com>
2021-05-24 06:44:56 +1000

    First update for react project2

--- react-admin_client/src/index.js
+++ react-admin_client/src/index.js
@@ -1,17 +1,10 @@
+//entry js
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//render App component into the index.html's <div> and show it on your page
+ReactDOM.render(<App/>, document.getElementById('root'));
