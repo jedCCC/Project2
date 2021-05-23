ee9b3d7262986a39333e1a04525b58f627c4a44b
jed <a535297316@gmail.com>
2021-05-24 07:43:12 +1000

    update config

--- react-admin_client/src/App.js
+++ react-admin_client/src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-
+import {message, Button} from 'antd';
 //root component
 
 export default class App extends Component{
 
-
+    handleClick=()=>{
+        message.success('this is a success message')
+        
+    }
     render(){
-        return <div>App</div>
+        return <Button type="primary" onClick={this.handleClick}>Primary</Button>
     }
 }
--- react-admin_client/src/index.js
+++ react-admin_client/src/index.js
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import '../node_modules/antd/dist/antd.css'
 
 import App from './App';
 
